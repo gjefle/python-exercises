1eaa5a265574f99b40ad32fe93644a24cc1375e4
Gjermund Fleten <gjermufl@gmail.com>
2016-09-23 21:54:44 +0200

    Exercise3

--- exercises/FM1115_Exercises/Chapter3.py
+++ exercises/FM1115_Exercises/Chapter3.py
@@ -1,31 +1,45 @@
 # -*- coding: utf-8 -*-
-# Chapter 3 - Computing integrals
+"""
+Created on Fri Sep 23 21:11:39 2016
 
-# Task 1 - Exercise from Fronter
-
-# Trapezoidal integral method
-def trapezoidal(f, a, b, n): 
-    # Resolution h
-    h = float(b-a)/n
-    # Calculate first and last values
-    sum = 0.5*(f(a) + f(b))
-    
-    # Iterate over values. 
-    # First (a) and last value (b) is skipped.
-    for i in range(1, n):
-        sum +=  f(a + i*h)
-    sum *= h
-    return sum
-
-def midpoint(f, a, b, n):
-    h = float(b-a)/n
-    for i in range(0, n):
-        x = float(a + h/2) + i*h
-        sum += f(x)        
-    sum *= h
-    return sum
-    
-    
-    
+@author: Gjermund
+"""
+from integral_methods import trapezoidal, midpoint
 
 
+#Chapter 3 - Exercises
+
+#Exercise 3.1 Trapezoidal vs Hand calculations
+start = 1
+end = 3
+f = lambda x: 2*x**3
+by_hand = 40
+print 'Exercise 1, 2'
+print 'By hand: %g, Using trapezoidal: %g' % (by_hand, trapezoidal(f,start, end, 2))
+
+# Exercise 3.2 Midpoint vs hand calculations
+print 'Using midpoint: %g' % (midpoint(f, start, end, 2))
+print ''
+
+# Exercise 3.3 Simple integral using trapezoidal and midpoint
+start = 2
+end = 6
+f = lambda x: x*(x-1)
+n = 2
+m = midpoint(f, start, end, n)
+em = abs(expected - m)
+t = trapezoidal(f, start, end, n)
+et = abs(expected - t)
+expected = 53.33333333333333
+print 'Exercise 3'
+print 'Using 2 intervals - Midpoint: %g (%g), trapezoidal: %g (%g)' % (m, em, t, et)
+n = 100
+m = midpoint(f, start, end, n)
+em = abs(expected - m)
+t = trapezoidal(f, start, end, n)
+et = abs(expected - t)
+print 'Using 100 intervals - Midpoint: %g (%g), trapezoidal: %g (%g)' % (m, em, t, et)
+print ''
+
+#Exercise 3.4
+  
---
+++ exercises/FM1115_Exercises/integral_methods.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+# Chapter 3 - Computing integrals
+
+# Task 1 - Exercise from Fronter
+
+# Trapezoidal integral method
+def trapezoidal(f, a, b, n): 
+    # Resolution h
+    h = float(b-a)/n
+    # Calculate first and last values
+    sum = 0.5*(f(a) + f(b))
+    
+    # Iterate over values. 
+    # First (a) and last value (b) is skipped.
+    for i in range(1, n):
+        sum +=  f(a + i*h)
+    sum *= h
+    return sum
+
+def midpoint(f, a, b, n):
+    h = float(b-a)/n
+    sum = 0
+    for i in range(0, n):
+        x = float(a + h/2) + i*h
+        sum += f(x)        
+    sum *= h
+    return sum
+
+# Unit tests
+def test_trapezoidal():    
+    f = lambda x: 90*x**2 - 3*x**4
+    start = 1
+    end = 20
+    resolution = 10000
+    expected = -1680029.4
+    tolerance = 0.1
+    result = trapezoidal(f, start, end, resolution)
+    diff = abs(expected - result)
+    assert diff < tolerance, 'diff: %g, tol: %g' % (diff, tolerance)
+    
+def test_midpoint():
+    f = lambda x: 90*x**2 - 3*x**4
+    start = 1
+    end = 20
+    resolution = 10000
+    expected = -1680029.4
+    tolerance = 0.1
+    result = midpoint(f, start, end, resolution)
+    diff = abs(expected - result)
+    assert diff < tolerance, 'diff: %g, tol: %g' % (diff, tolerance)
+    
+test_trapezoidal()
+test_midpoint()
+        
+    
+
+
